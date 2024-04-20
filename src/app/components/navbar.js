25a59b163902a955980d94696fc7b76e2984e982
erinkwon01 <erinkwon01@g.ucla.edu>
2024-04-20 00:11:06 -0700

    Working

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import Logo from "./Logo";
-import Button from "./Button";
 
 const NavBar = () => {
   return (
@@ -9,7 +7,6 @@ const NavBar = () => {
       <div className="w-full h-20 bg-emerald-800 sticky top-0">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
-            <Logo />
             <ul className="hidden md:flex gap-x-6 text-white">
               <li>
                 <Link href="/about">
@@ -27,7 +24,6 @@ const NavBar = () => {
                 </Link>
               </li>
             </ul>
-            <Button />
           </div>
         </div>
       </div>
